b8f2b783df1d321f74017025470cd99ca10d7eb2
Alexander Martinez <alexander.martinez@twiinshrm.com>
2024-04-19 17:31:40 -0500

    NEW: Inicializacion de proyecto Nodejs

---
+++ app.js
@@ -0,0 +1,23 @@
+// Importar el módulo http de Node.js
+const http = require('http');
+
+// Definir el manejador de solicitudes (request handler)
+const requestHandler = (request, response) => {
+  // Configurar el encabezado de la respuesta con el código de estado 200 (OK) y el tipo de contenido como texto plano
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+  // Enviar el mensaje de respuesta al cliente
+  response.end('¡Hola, mundo!\n');
+};
+
+// Crear un servidor HTTP y pasarle el manejador de solicitudes
+const server = http.createServer(requestHandler);
+
+// Escuchar las solicitudes en el puerto 3000 y la dirección IP 'localhost'
+server.listen(3000, 'localhost', (err) => {
+  // Si hay un error al iniciar el servidor, mostrar un mensaje de error
+  if (err) {
+    return console.log('Error: ', err);
+  }
+  // Si no hay errores, mostrar un mensaje indicando que el servidor está escuchando en el puerto 3000
+  console.log('Servidor escuchando en: http://localhost:3000/');
+});
