9ea52c9ca108ce588cb5bbbb29ac5e594cb41ebd
Alexis Sangoquiza <77704878+AlexisSangoquiza@users.noreply.github.com>
2024-04-30 07:49:53 -0500

    Obtener parametros del config

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 // Importar el módulo http de Node.js
 const http = require('http');
 
+// Lee el archivo de configuración
+const config = JSON.parse(fs.readFileSync('config.json'));
+
 // Definir el manejador de solicitudes (request handler)
 const requestHandler = (request, response) => {
   // Configurar el encabezado de la respuesta con el código de estado 200 (OK) y el tipo de contenido como texto plano
@@ -12,12 +15,16 @@ const requestHandler = (request, response) => {
 // Crear un servidor HTTP y pasarle el manejador de solicitudes
 const server = http.createServer(requestHandler);
 
+// Usa el puerto obtenido del archivo de configuración
+const puerto = config.puerto;
+const direccion = config.host;
+
 // Escuchar las solicitudes en el puerto 3000 y la dirección IP 'localhost'
-server.listen(3000, 'localhost', (err) => {
+server.listen(puerto, direccion, (err) => {
   // Si hay un error al iniciar el servidor, mostrar un mensaje de error
   if (err) {
     return console.log('Error: ', err);
   }
   // Si no hay errores, mostrar un mensaje indicando que el servidor está escuchando en el puerto 3000
-  console.log('Servidor escuchando en: http://localhost:3000/');
+    console.log(`Servidor escuchando en: http://${direccion}:${puerto}/`);
 });
